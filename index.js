0c566f87a9030065ae33b5e5669194fa99d38079
tahdi-islam <tshdi60@gmail.com>
2022-10-20 19:48:19 +0600

    m

--- index.js
+++ index.js
@@ -15,12 +15,33 @@ app.get('/', (req, res) => {
     res.send('news api running')
 });
 
-app.get('/news-categories', (req, res) => {
+app.get('/category', (req, res) => {
     res.send(categories)
 })
 
+// specific news category by id
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    // conditional data
+    if(id === '08'){
+        res.send(news)
+    }else{
+        const selectedCategory = news.filter(n => n.category_id === id)
+        res.send(selectedCategory)
+    }
+})
+
+// all news
 app.get('/news', (req, res) => {
     res.send(news)
+})
+
+// specific news by id
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+    // console.log(req);
 })
 
 app.listen(port, () => {
